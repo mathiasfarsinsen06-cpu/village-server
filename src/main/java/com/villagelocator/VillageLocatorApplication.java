6c24d7bfb3bfa29aa44780536b5601d824af5e2d
mathiasfarsinsen06-cpu <mathiasfarsinsen06@gmail.com>
2026-06-03 17:05:07 +0200

    Add ComponentScan to enable Spring to find controllers and services

--- src/main/java/com/villagelocator/VillageLocatorApplication.java
+++ src/main/java/com/villagelocator/VillageLocatorApplication.java
@@ -2,8 +2,10 @@ package com.villagelocator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.villagelocator")
 public class VillageLocatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(VillageLocatorApplication.class, args);
